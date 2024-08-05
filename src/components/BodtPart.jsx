4b17c97b34dd0a9fc657ba38a494405fdccda6e8
Elma Narimani <narimanielma@gmail.com>
2024-08-05 17:03:55 +0330

    adding some props

---
+++ src/components/BodtPart.jsx
@@ -0,0 +1,9 @@
+import { Stack, Typography } from "@mui/material"
+
+const BodtPart = () => {
+  return (
+    <div>BodtPart</div>
+  )
+}
+
+export default BodtPart
--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,11 +1,21 @@
-import { Box } from "@mui/material"
+import { Box } from "@mui/material";
+import BodtPart from "./BodtPart";
 
-const HorizontalScrollbar = ({data}) => {
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <div>{data.map((item)=>(
-<Box key={item.id || item}></Box>
-    ))}</div>
-  )
-}
+    <div>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m="0 40px"
+        >
+          <BodtPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+        </Box>
+      ))}
+    </div>
+  );
+};
 
-export default HorizontalScrollbar
+export default HorizontalScrollbar;
